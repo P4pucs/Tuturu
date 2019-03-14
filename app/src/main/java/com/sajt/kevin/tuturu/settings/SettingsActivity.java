255336e7df341040326d6adb8305d5a2a4797a8e
P4pucs <kovacs.kevin98@gmail.com>
2019-03-14 23:09:18 +0100

    sajt

--- app/src/main/java/com/sajt/kevin/tuturu/settings/SettingsActivity.java
+++ app/src/main/java/com/sajt/kevin/tuturu/settings/SettingsActivity.java
@@ -1,9 +1,18 @@
 package com.sajt.kevin.tuturu.settings;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
 import com.sajt.kevin.tuturu.R;
 
@@ -20,6 +29,52 @@ public class SettingsActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            bindSummaryValue(findPreference("key_full_name"));
+            bindSummaryValue(findPreference("key_email"));
+            bindSummaryValue(findPreference("key_sleep_timer"));
+            bindSummaryValue(findPreference("key_music_quality"));
+            bindSummaryValue(findPreference("key_notifications_ringtone"));
         }
     }
+
+    private static void bindSummaryValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(listener);
+        listener.onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                .getString(preference.getKey(), ""));
+    }
+
+    private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                // set the summary to reflect the new value
+                preference.setSummary(index >= 0
+                        ? listPreference.getEntries()[index]
+                        : null);
+            } else if (preference instanceof EditTextPreference) {
+                preference.setSummary(stringValue);
+            } else if (preference instanceof RingtonePreference) {
+                if (TextUtils.isEmpty(stringValue)) {
+                    // no ringtone
+                    preference.setSummary("Silent");
+                } else {
+                    Ringtone ringtone = RingtoneManager.getRingtone(preference.getContext(), Uri.parse(stringValue));
+
+                    if (ringtone == null) {
+                        //clear the summary
+                        preference.setSummary("choose notification ringtone");
+                    } else {
+                        String name = ringtone.getTitle(preference.getContext());
+                        preference.setSummary(name);
+                    }
+                }
+            }
+            return true;
+        }
+    };
 }
