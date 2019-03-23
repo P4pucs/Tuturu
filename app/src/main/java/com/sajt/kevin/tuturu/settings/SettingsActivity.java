cf1dd872466e3bbe53ef7a4dd55e85cc51cb6cb5
P4pucs <kovacs.kevin98@gmail.com>
2019-03-23 16:41:16 +0100

    sajt

--- app/src/main/java/com/sajt/kevin/tuturu/settings/SettingsActivity.java
+++ app/src/main/java/com/sajt/kevin/tuturu/settings/SettingsActivity.java
@@ -1,10 +1,16 @@
 package com.sajt.kevin.tuturu.settings;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.sajt.kevin.tuturu.R;
 import com.sajt.kevin.tuturu.audio.Magic;
@@ -14,15 +20,19 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SettingsActivity extends PreferenceActivity {
 
-    public SwitchPreference magicPref;
+    final int REQUEST_PERMISSION_CODE = 1000;
+
     static AtomicBoolean run = new AtomicBoolean(false);
-    Thread magicThread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new MainSettingsFragment()).commit();
+
+        if (checkPermissionFromDevice()) {
+        //RECORDER 111111111111111111
 
         new Thread(()-> {
 
@@ -46,6 +56,10 @@ public class SettingsActivity extends PreferenceActivity {
             }
         }).start();
 
+        } else {
+            requestPermission();
+        }
+
     }
 
     public static class MainSettingsFragment extends PreferenceFragment {
@@ -58,9 +72,11 @@ public class SettingsActivity extends PreferenceActivity {
             SwitchPreference magicSwitch = (SwitchPreference) findPreference("magic_key");
 
             if (magicSwitch.isEnabled()) {
-                run.set(magicSwitch.isEnabled());
+                run.set(true);
+                System.out.println("TRUE Blyat");
             } else {
-                run.set(magicSwitch.isEnabled());
+                System.out.println(" FALSE Blyat");
+                run.set(false);
             }
 
             if (magicSwitch != null) {
@@ -68,13 +84,43 @@ public class SettingsActivity extends PreferenceActivity {
                     boolean isMagicOn = (Boolean) isMagicOnObject;
                     System.out.println("isMagicOn: " + isMagicOn);
                     if (isMagicOn) {
-                        run.set(isMagicOn);
+                        run.set(true);
                     } else {
-                        run.set(isMagicOn);
+                        run.set(false);
                     }
                     return true;
                 });
             }
+        }
+    }
+
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO
+        }, REQUEST_PERMISSION_CODE);
+    }
+
+    private boolean checkPermissionFromDevice() {
+        int write_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+
+        return write_external_storage_result == PackageManager.PERMISSION_GRANTED &&
+                record_audio_result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(SettingsActivity.this,"Permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SettingsActivity.this,"Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
         }
     }
 
